2acf639414f3fa808d470151a6d06683524bd87d
areej47 <102627714+areej47@users.noreply.github.com>
2022-03-29 22:09:01 +0100

    Add files via upload

---
+++ User.java
@@ -0,0 +1,12 @@
+public class User {
+    public int id;
+    public String name;
+    public String email;
+    public String phone;
+    public int sex;
+    public String userSex;
+    public int age;
+    public String password;
+    public int role;
+    public String userRole;
+}
