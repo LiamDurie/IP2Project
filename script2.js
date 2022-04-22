9c3dbb756c5fbb53a2a29c1a6c7ecad4f60af4b8
areej206 <99836322+areej206@users.noreply.github.com>
2022-04-22 03:58:32 +0100

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,91 @@
+/**
+ function mindfulMinus() {
+    var x = document.getElementsByTagName("mindfulTable");
+    console.log(x); 
+}
+ */
+/**
+function removeFunction(){
+    let p = document.getElementById("physiological");
+    p.remove()
+}
+*/
+
+function removeFunction(){
+    let a = document.getElementById("heart");
+    a.remove()
+}
+
+function removeFunction2(){
+    let b = document.getElementById("blood_pressure");
+    b.remove()
+}
+
+function removeFunction3(){
+    let c = document.getElementById("blood_temperature");
+    c.remove()
+}
+function removeFunction4(){
+    let d = document.getElementById("blood_oxygen");
+    d.remove()
+}
+
+function removeFunction5(){
+    let e = document.getElementById("respiration_rate");
+    e.remove()
+}
+
+function removeFunction7(){
+    let e = document.getElementById("report");
+    e.remove()
+}
+
+function removeFunction8(){
+    let f = document.getElementById("mindfulness");
+    f.remove()
+}
+
+function removeFunction10(){
+    let f = document.getElementById("meditation");
+    f.remove()
+}
+
+function removeFunction11(){
+    let f = document.getElementById("Massage");
+    f.remove()
+}
+
+function removeFunction12(){
+    let f = document.getElementById("pets");
+    f.remove()
+}
+
+function removeFunction13(){
+    let f = document.getElementById("Walk");
+    f.remove()
+}
+
+function removeFunction14(){
+    let f = document.getElementById("Yoga");
+    f.remove()
+}
+
+function removeFunction15(){
+    let f = document.getElementById("Drawing");
+    f.remove()
+}
+
+function removeFunction16(){
+    let f = document.getElementById("Journal");
+    f.remove()
+}
+
+function removeFunction17(){
+    let f = document.getElementById("off");
+    f.remove()
+}
+
+function removeFunction18(){
+    let f = document.getElementsByClassName("plan");
+    f.remove()
+}
